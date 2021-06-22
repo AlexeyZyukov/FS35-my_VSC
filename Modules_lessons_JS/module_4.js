8a61074c19e224be006ef1b75b4e107e03513acd
Alexey Zyukov <aalex64@ukr.net>
2021-06-22 23:01:19 +0300

    module 4 completed

--- Modules_lessons_JS/module_4.js
+++ Modules_lessons_JS/module_4.js
@@ -102,7 +102,7 @@
 //пример из учебника:
 // function greetGuest(greeting) {
 //     console.log(`${greeting}, ${this.username}.`);
-    
+
 // }
 
 // const mango = { username: "Манго" };
@@ -130,9 +130,9 @@
 //    return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`
 // }
 // const messages = [];
-    
+
 //     for (let i = 0; i < orders.length; i += 1) {
-       
+
 //         let result = composeMessage.call(orders[i], i + 1);
 //         console.log(result)
 //         messages.push(result);
@@ -146,7 +146,7 @@
 //         messageResult = (`Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`);
 //         console.log(messageResult);        
 // };
-       
+
 // for (let i = 0; i < orders.length; i += 1) {
 //     composeMessage.call(orders[i], i + 1);
 //     messages.push(messageResult)
@@ -156,20 +156,80 @@
 
 
 //№ 8-10 Метод apply
-const orders = [
-  { email: "solomon@topmail.ua", dish: "Burger" },
-  { email: "artemis@coldmail.net", dish: "Pizza" },
-  { email: "jacob@mail.com", dish: "Taco" },
-];
+// const orders = [
+//   { email: "solomon@topmail.ua", dish: "Burger" },
+//   { email: "artemis@coldmail.net", dish: "Pizza" },
+//   { email: "jacob@mail.com", dish: "Taco" },
+// ];
 
-// Пиши код ниже этой строки
-function composeMessage(position) {
-  return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
-}
+// // Пиши код ниже этой строки
+// function composeMessage(position) {
+//   return `Готовим ${this.dish} для ${this.email}. Ваш заказ ${position}-й в очереди.`;
+// }
 
-const messages = [];
-for (let i = 0; i < orders.length; i++) {
-  const msg = composeMessage.apply(orders[i], [i + 1]);
-  messages.push(msg);
-}
-console.log(messages);
+// const messages = [];
+// for (let i = 0; i < orders.length; i++) {
+//   const msg = composeMessage.apply(orders[i], [i + 1]);
+//   messages.push(msg);
+// }
+// console.log(messages);
+
+//# 9-10 Метод bind
+//Метод bind создаёт и возвращает копию функции foo с привязанным контекстом obj и аргументами arg1, 
+//arg2 и т.д.Получается копия функции которую можно передать куда угодно и вызвать когда угодно.
+// const pizzaPalace = {
+//   company: 'Pizza Palace',
+// };
+
+// const burgerShack = {
+//   company: 'Burger Shack',
+// };
+
+// function composeMessage(customerName) {
+//   return `${customerName}, всегда рады вас видеть в «${this.company}».`;
+// }
+// // Пиши код ниже этой строки
+
+// const pizzaPalaceComposer = composeMessage.bind(pizzaPalace);
+// const pizzaPalaceMessage = pizzaPalaceComposer('Манго');
+
+// const burgerShackComposer = composeMessage.bind(burgerShack);
+// const burgerShackMessage = burgerShackComposer('Поли');
+
+//# 10-10 Метод bind и методы объекта
+//При передаче методов объекта как колбэк-функций, контекст не сохраняется. Колбэк это ссылка на 
+//метод, которая присваивается как значение параметра, вызываемого без объекта.
+//Метод bind используется для привязки контекста при передаче методов объекта как колбэк-функций. 
+//Передадим колбэком не оригинальный метод getFullName, а его копию с привязанным контекстом к объекту customer.
+// const service = {
+//   mailingList: ['mango@mail.com', 'poly@hotmail.de', 'ajax@jmail.net'],
+//   subscribe(email) {
+//     this.mailingList.push(email);
+//     return `Почта ${email} добавлена в рассылку.`;
+//   },
+//   unsubscribe(email) {
+//     this.mailingList = this.mailingList.filter((e) => e !== email);
+//     return `Почта ${email} удалена из рассылки.`;
+//   },
+// };
+
+// function logAndInvokeAction(email, action) {
+//   console.log(`Выполняем действие с ${email}.`);
+//   return action(email);
+// }
+
+// const firstInvoke = logAndInvokeAction('kiwi@mail.uk', service.subscribe.bind(service));
+// console.log(firstInvoke);
+// // Почта kiwi@mail.uk добавлена в рассылку.
+
+// console.log(service.mailingList);
+// /* ['mango@mail.com', 
+//     'poly@hotmail.de', 
+//     'ajax@jmail.net', 
+//     'kiwi@mail.uk']*/
+// const secondInvoke = logAndInvokeAction('poly@hotmail.de', service.unsubscribe.bind(service));
+// console.log(secondInvoke);
+// // Почта poly@hotmail.de удалена из рассылки.
+
+// console.log(service.mailingList); // ['mango@mail.com', 'ajax@jmail.net', 'kiwi@mail.uk']
+
--- Modules_lessons_JS/module_5.js
+++ Modules_lessons_JS/module_5.js
@@ -19,3 +19,86 @@
 
 // console.log(hat.updateColor);
 // updateColor("orange");
+
+
+
+
+// const Car = function ({ brand, model, price } = {}) {
+//     //console.log("this is", this);
+//     //const {brand, model, price} = config;
+//     this.brand = brand;
+//     this.model = model;
+//     this.price = price;
+//     //console.log("this is", this);
+// };
+
+// Car.prototype.changePrice = function (newPrice) {
+//     this.price = newPrice;
+// };
+
+// const myCar = new Car({
+//     brand: "Opel",
+//     model: "Astra",
+//     price: 15000,
+// });
+
+// myCar.changePrice(5000);
+// console.log("myCar", myCar);
+
+// const myCar2 = new Car();
+// console.log("myCar2", myCar2);
+
+// const ProductCard = function ({ productName, piecePrice, quantity } = {}) {
+//     this.productName = productName;
+//     this.piecePrice = piecePrice;
+//     this.quantity = quantity;
+// };
+
+// ProductCard.prototype.changePrice = function (newpiecePrice) {
+//     this.piecePrice = newpiecePrice;
+// };
+
+// ProductCard.prototype.changeQuantity = function (newQuantity) {
+//     this.quantity = newQuantity;
+// };
+
+// const myProduct = new ProductCard({ productName: "fork", quantity: 10, piecePrice: 100, })
+// console.log(myProduct);
+
+// myProduct.changeQuantity(20);
+// console.log(myProduct);
+
+//# 1-19 Прототип объекта и метод Object.create()
+// const parent = {
+//     name: 'Stacey',
+//     surname: 'Moore',
+//     age: 54,
+//     heritage: 'Irish'
+// };
+// // Пиши код ниже этой строки
+
+// const child = Object.create(parent);
+
+// // Пиши код выше этой строки
+// child.name = 'Jason';
+// child.age = 27;
+
+//# 2-19 Цепочка прототипов
+// const ancestor = {
+//     name: 'Paul',
+//     age: 83,
+//     surname: 'Dawson',
+//     heritage: 'Irish'
+// };
+// // Пиши код ниже этой строки
+
+// const parent = Object.create(ancestor);
+// parent.name = 'Stacey';
+// parent.surname = 'Moore';
+// parent.age = 54;
+
+// const child = Object.create(parent);
+// child.name = 'Jason';
+// child.age = 27;
+
+// // Пиши код выше этой строки
